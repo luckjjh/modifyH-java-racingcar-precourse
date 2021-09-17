8154336b082b49e7021d69755bf12d0079a1112b
modifyH <luckjjh1@naver.com>
2021-09-17 17:11:40 +0900

    예외처리 수정

--- modifyH-java-racingcar-precourse/src/main/java/racingcar/Application.java
+++ modifyH-java-racingcar-precourse/src/main/java/racingcar/Application.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        String num = "2.31";
+        System.out.println(Integer.parseInt(num));
         RacingGame racingGame = new RacingGame();
         racingGame.startRacingGame();
     }
